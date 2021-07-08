bf12f1eaec80e86310956457b674d1181c0cc226
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-07-08 20:54:38 +0800

    Update arithmetic sequence.py

--- arithmetic sequence.py
+++ arithmetic sequence.py
@@ -5,26 +5,20 @@ a = [5, 7, 9, 11, 13]
 if len(a) <= 1:
     print(False)
 else:
-    # take first and second variable from the list
     x = a[0]
     y = a[1]
 
-    # make a new list to compare it with the first list
     b = [x]
 
-    # find the difference
     z = y - x
     p = len(a)
 
-    # fill the new list with the actual arithmetic sequence by taking the first variable and keep adding it with the difference
     for i in range(p - 1):
         x = x + z
         b.append(x)
 
-    # compare the two list
     c = list(set(a) - set(b))
 
-    # check if there is a difference between the first and the second list
     if len(c) == 0:
         print(True)
     else:
